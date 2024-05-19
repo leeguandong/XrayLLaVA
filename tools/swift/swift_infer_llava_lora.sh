ebc14cb7ec7645f1767ad9575b2144f976855446
KUN <leeguandon@gmail.com>
2024-05-19 20:03:04 +0800

    update

---
+++ tools/swift/swift_infer_llava_lora.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=0 swift infer \
+    --ckpt_dir /home/image_team/image_team_docker_home/lgd/e_commerce_lmm/results/llava16_swift_xray/llava1d6-mistral-7b-instruct/v16-20240513-024750/checkpoint-1980/ \
+    --load_dataset_config true
